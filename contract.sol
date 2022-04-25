f95f77a42a38c8c0a6ef40c3223ee761f3ac719c
Josh Bishop <joshuabishop@protonmail.com>
2022-04-25 09:00:28 -0400

    contract buyable

--- contract.sol
+++ contract.sol
@@ -7,22 +7,23 @@ contract Lottery{
     address[] public winners;
     uint public pool = 0;
 
-    uint public constant CAP_AMOUNT = 100;
-    uint public constant PRICE = 1;
+    uint public constant CAP_AMOUNT = 1 ether;
+    uint public constant PRICE = 1 ether;
 
     function getPool() public view returns (uint) {
         return pool;
     }
 
-    function enter() public payable{
+    function enter() public payable {
         require(address(this).balance >= msg.value, "Address: insufficient balance");
         require(msg.value >= 1 ether, "Must enter with at least 1 MATIC");
-        pool += msg.value;
-        //for(uint i = 0; i < msg.value; i++) {
-        //    players.push(msg.sender);
-        //}
+        uint bal = msg.value / 10**18;
+        pool += bal;
+        for(uint i = 0; i < bal; i++) {
+            players.push(msg.sender);
+        }
         if(pool >= CAP_AMOUNT) {
-            //pickWinner();
+            pickWinner();
         }
     }
 
